0396f85cd091c93511a68a583f0077466f146d6d
Noémie Houdou <houdou.noemie@gmail.com>
2023-01-27 21:02:42 +0100

    feat: add main layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,16 +1,39 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import styles from '../../styles/_Layout.module.scss';
+import logo from '../../assets/images/logo.png';
+import logoFooter from '../../assets/images/logo_footer.png';
 
 function Layout({ children }) {
   return (
-    <div>
-      <header>
+    <div className={styles.layout}>
+      <header className={styles.header}>
+        <img src={logo} alt="Logo de Kasa rouge" />
         <nav>
-          <Link to="/">Accueil</Link>
-          <Link to="/about">À propos</Link>
+          <ul className={styles.header__nav}>
+            <li>
+              <NavLink
+                className={(link) => (link.isActive ? styles.active : '')}
+                to="/"
+              >
+                Accueil
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={(link) => (link.isActive ? styles.active : '')}
+                to="/about"
+              >
+                À propos
+              </NavLink>
+            </li>
+          </ul>
         </nav>
       </header>
-      {children}
-      <footer>Logo et copyrigth</footer>
+      <main>{children}</main>
+      <footer className={styles.footer}>
+        <img src={logoFooter} alt="Logo de Kasa blanc" />
+        <p>© 2020 Kasa. All rights reserved</p>
+      </footer>
     </div>
   );
 }
