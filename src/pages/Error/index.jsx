4f827325c483869096830adff159c94d07b99e0e
Noémie Houdou <houdou.noemie@gmail.com>
2023-02-01 20:04:41 +0100

    feat: layout error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,5 +1,18 @@
+import error from '../../assets/images/error.jpg';
+import { Link } from 'react-router-dom';
+import styles from '../../styles/_Error.module.scss';
+
 function Error() {
-  return <div>Erreur 404</div>;
+  return (
+    <section className={styles.error}>
+      <img src={error} alt="Error" />
+      <h1>
+        Oups! La page que
+        <br /> vous demandez n'existe pas.
+      </h1>
+      <Link to="/">Retourner sur la page d'accueil</Link>
+    </section>
+  );
 }
 
 export default Error;
